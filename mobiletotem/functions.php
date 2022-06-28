1f2de8b66b032d63be898fbbc416737e9549ffee
j.w. newman <57886781+wade217@users.noreply.github.com>
2022-06-28 04:12:11 -0500

    Add files via upload

---
+++ mobiletotem/functions.php
@@ -0,0 +1,56 @@
+<?php
+// Exit if accessed directly
+if ( !defined( 'ABSPATH' ) ) exit;
+
+// BEGIN ENQUEUE PARENT ACTION
+// AUTO GENERATED - Do not modify or remove comment markers above or below:
+
+if ( !function_exists( 'chld_thm_cfg_locale_css' ) ):
+    function chld_thm_cfg_locale_css( $uri ){
+        if ( empty( $uri ) && is_rtl() && file_exists( get_template_directory() . '/rtl.css' ) )
+            $uri = get_template_directory_uri() . '/rtl.css';
+        return $uri;
+    }
+endif;
+add_filter( 'locale_stylesheet_uri', 'chld_thm_cfg_locale_css' );
+         
+if ( !function_exists( 'child_theme_configurator_css' ) ):
+    function child_theme_configurator_css() {
+        wp_enqueue_style( 'chld_thm_cfg_child', trailingslashit( get_stylesheet_directory_uri() ) . 'style.css', array( 'twentytwentytwo-style','twentytwentytwo-style' ) );
+    }
+endif;
+add_action( 'wp_enqueue_scripts', 'child_theme_configurator_css', 10 );
+
+// END ENQUEUE PARENT ACTION
+
+// date shortcodes //
+function year_shortcode () {
+$year = date_i18n ('Y');
+return $year;
+}
+add_shortcode ('year', 'year_shortcode');
+
+function month_shortcode () {
+$monthyear = date_i18n ('F');
+return $month;
+}
+add_shortcode ('month', 'month_shortcode'); 
+
+function yyyymmdd_shortcode () {
+$yyyymmdd = date_i18n ('y-m-d'); return $yyyymmdd;
+}
+add_shortcode ('yyyymmdd', 'yyyymmdd_shortcode');
+
+function monthyear_shortcode () {
+$monthyear = date_i18n ('F Y');
+return $monthyear;
+}
+add_shortcode ('monthyear', 'monthyear_shortcode');
+
+function day_shortcode () {
+$day = date_i18n ('l');
+return $day;
+}
+add_shortcode ('day', 'day_shortcode');
+
+// date shortcodes END //
